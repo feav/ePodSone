aee36471bdce05c1f290ca8fd76b7575bf896bb5
Armel E <armelem@ausiteodit.com>
2020-05-07 18:00:44 +0100

    mise en forme de l admin

--- src/Form/AbonnementType.php
+++ src/Form/AbonnementType.php
@@ -6,15 +6,34 @@ use App\Entity\Abonnement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class AbonnementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('start')
-            ->add('end')
-            ->add('state')
+            ->add('start', DateTimeType::class, [
+                'input'=>'datetime',
+                'widget' => 'single_text',
+                'html5' => false,
+                'attr' => ['class' => 'dateTimeFlatpickr form-control'],
+            ])
+            ->add('end', DateTimeType::class, [
+                'input'=>'datetime',
+                'widget' => 'single_text',
+                'html5' => false,
+                'attr' => ['class' => 'dateTimeFlatpickr form-control'],
+            ])
+            ->add('state', ChoiceType::class, [
+                'choices' => [
+                    'Type de reduction' => [
+                        'Impaye' => 0,
+                        'Paye' => 1,
+                    ]
+                ],
+            ])
             ->add('formule')
             ->add('panier')
             ->add('user')
