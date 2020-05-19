a9bd75c3c220dbd9595d06c2c9f9118ca71e5223
alex <azebaze@ausiteodit.com>
2020-05-19 10:21:56 +0200

    cart disign

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -291,9 +291,6 @@ class HomeController extends AbstractController
                     ';' // The delimiter
                 );
             }
-            //while($row = $results->fetch()) {
-                
-            //}
 
             fclose($handle);
         });
@@ -304,33 +301,5 @@ class HomeController extends AbstractController
 
         return $response;
 
-    }
-
-    public function generateCsvAction()
-    {
-        $response = new StreamedResponse();
-        $response->setCallback(function() {
-            $handle = fopen('php://output', 'w+');
-
-            // Add the header of the CSV file
-            fputcsv($handle, array('Name', 'Surname', 'Age', 'Sex'),';');
-            // Query data from database
-            // Add the data queried from database
-            while($row = $results->fetch()) {
-                fputcsv(
-                    $handle, // The file pointer
-                    array($row['name'], $row['surname'], $row['age'], $row['sex']), // The fields
-                    ';' // The delimiter
-                );
-            }
-
-            fclose($handle);
-        });
-
-        $response->setStatusCode(200);
-        $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
-        $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
-
-        return $response;
     }
 }
